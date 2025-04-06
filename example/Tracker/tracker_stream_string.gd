e5a806467b10c72b145c2e3151abc3109432071f
CNTW_DEV <homiedeveloperscontact@gmail.com>
2025-04-06 09:46:27 -0400

    Comments

--- example/Tracker/tracker_stream_string.gd
+++ example/Tracker/tracker_stream_string.gd
@@ -2,9 +2,10 @@ extends Node
 
 var my_grant_value_string : TrackerStream = TrackerStream.new()
 
+# Buffer Track
 var track : Array = []
 
-## On update callback for save track!
+## Success Update callback
 func _on_update_int(last_value : Variant, new_value : Variant) -> void:
 	# Get last tracked value
 	if track.size() > 0:
@@ -34,7 +35,7 @@ func _ready() -> void:
 
 	#INTIIALZIE
 	print("Before initialize: is string ", my_grant_value_string.get_type() == TYPE_STRING)
-	my_grant_value_string.value = "Any thing" #First time defined this tracker as integer
+	my_grant_value_string.value = "Any thing" #First time defined this tracker as string
 	print("After initialize: is string ", my_grant_value_string.get_type() == TYPE_STRING)
 
 	my_grant_value_string.value = 1.4 # Trigger error by another type by default
