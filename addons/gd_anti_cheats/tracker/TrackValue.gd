591929260eaa87c022c3f1511b8d253d8963041c
CNTW_DEV <homiedeveloperscontact@gmail.com>
2025-05-15 01:44:04 -0400

    0.2.1

--- addons/gd_anti_cheats/tracker/TrackValue.gd
+++ addons/gd_anti_cheats/tracker/TrackValue.gd
@@ -1,5 +1,11 @@
 class_name TrackValue extends RefCounted
-const MAX_TRACK_VALUES : int = 10
+var max_track_value : int = 10:
+	set(max_value):
+		max_track_value = maxi(max_value, 0)
+		if _buffer_track.size() > 0:
+			_buffer_track.resize(max_track_value)
+			_time_track.resize(max_track_value)
+		
 
 ## Emitted when value _buffer_track is updated.
 signal updated(last_value : Variant, new_value : Variant)
@@ -19,23 +25,110 @@ var value : Variant:
 			last_value_error.emit(_buffer_track[_buffer_track.size() - 1], _track_stream.value)
 		return _track_stream.value
 
+var _index_track : int = 0
+
 var _buffer_track : Array[Variant] = []:
 	set(__):
 		return
-		
+
+var _time_track : PackedFloat64Array = []:
+	set(__):
+		return
 
 var _track_stream : TrackerStream = TrackerStream.new():
 	set(__):
 		return
 
-## Get Tracked List.
+## Get Full Track Buffer List.
 func get_track() -> Array[Variant]:
+	var track : Array[Variant] = Array(_buffer_track)
+	for x : int in range(_buffer_track.size()):
+		track[x] =  _buffer_track[x]
 	return _buffer_track.duplicate(true)
+	
+## Get Sorter Current Tracked List By Time Updated.
+func get_sorter_track() -> Array[Variant]:
+	var track : Array[Array]
+	var out : Array[Variant]
+	
+	var total : int = 0
+	var size : int = _buffer_track.size()
+	
+	track.resize(size)
+	out.resize(size)
+	
+	for x : int in range(size):
+		var time : float = _time_track[x]
+		if time == 0.0:
+			break
+		total += 1
+		track[x] = [_buffer_track[x], time]
+		
+	if total < track.size():
+		track.resize(total)
+		out.resize(total)
+		
+	track.sort_custom(_sort_ascending)
+	
+	for x : int in range(track.size()):
+		out[x] = track[x][0]
+		
+	return out
+	
+##Get Current Track With Time Unix Parsed.
+func get_log_track_time_parsed() -> Array[Variant]:
+	var track : Array[Array]
+	var total : int = 0
+	track.resize(_buffer_track.size())
+	
+	for x : int in range(_buffer_track.size()):
+		var time : float = _time_track[x]
+		if time == 0.0:
+			break
+		total += 1
+		track[x] = [_buffer_track[x], time]
+	
+	if total < track.size():
+		track.resize(total)
+		
+	track.sort_custom(_sort_ascending)
+	
+	for x : int in range(track.size()):
+		track[x][1] = _convert_time(track[x][1])
+		
+	return track
+	
+##Get Current Track With Unix Time.
+func get_log_track() -> Array[Variant]:
+	var track : Array[Array]
+	var total : int = 0
+	
+	track.resize(_buffer_track.size())
+	
+	for x : int in range(_buffer_track.size()):
+		var time : float = _time_track[x]
+		if time == 0.0:
+			break
+		total += 1
+		track[x] = [_buffer_track[x], _time_track[x]]
+	
+	if total < track.size():
+		track.resize(total)
+		
+	track.sort_custom(_sort_ascending)
+		
+	return track
+	
+func _convert_time(unix_time : float) -> String:
+	return Time.get_datetime_string_from_datetime_dict(Time.get_datetime_dict_from_unix_time(unix_time), true)
+	
+func _sort_ascending(val0 : Array, val1 : Array) -> bool:
+	return val0[1] < val1[1]
 	
 ## Get Last Tracked Value.
 func get_last_tracked_value() -> Variant:
 	if _buffer_track.size() > 0:
-		return _buffer_track[_buffer_track.size() - 1]
+		return _buffer_track[wrapi(_index_track - 1, 0, _buffer_track.size())]
 	if OS.is_debug_build():
 		push_warning("Not has a tracked value!")
 	return null
@@ -43,16 +136,20 @@ func get_last_tracked_value() -> Variant:
 func _on_updated(last_value : Variant, new_value : Variant) -> void:
 	# Get last tracked value
 	if _buffer_track.size() > 0:
-		if _buffer_track[_buffer_track.size() - 1] != last_value:
+		if _buffer_track[wrapi(_index_track - 1, 0, _buffer_track.size())] != last_value:
 			#External altered value!
 			last_value_error.emit(last_value, _buffer_track[_buffer_track.size() - 1])
+	elif max_track_value > 0:
+		_time_track.resize(max_track_value)
+		_buffer_track.resize(max_track_value)
+	else:
+		updated.emit(last_value, new_value)
+		return
 
 	# Add new value to _buffer_track
-	_buffer_track.append(new_value)
-
-	# Remove overflow stack
-	if _buffer_track.size() > MAX_TRACK_VALUES:
-		_buffer_track.remove_at(0)
+	_buffer_track[_index_track] = new_value
+	_time_track[_index_track] = Time.get_unix_time_from_system()
+	_index_track = wrapi(_index_track + 1, 0, _buffer_track.size())
 		
 	# Emit Propagation
 	updated.emit(last_value, new_value)
@@ -65,6 +162,5 @@ func _on_error(last_value : Variant, new_value : Variant) -> void:
 	error.emit(last_value, new_value)
 
 func _init() -> void:
-	_track_stream = TrackerStream.new()
 	_track_stream.updated.connect(_on_updated)
 	_track_stream.error.connect(_on_error)
